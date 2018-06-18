550e08c56f9879b04cf13d179c38c2105f94bbe1
DragonAK <danielfischeraroeira@gmail.com>
2018-06-18 14:52:43 -0300

    AY

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -6,9 +6,17 @@ def show
 end
 
 def new
+	@doc = Doc.new
 end
 
 def create
+	@doc = Doc.new(doc_params)
+
+	if @doc.save
+		redirect_to @doc
+	else
+	render 'new'
+	end	
 end
 
 def edit
@@ -26,6 +34,7 @@ def find_doc
 end
 
 def doc_params
+	params.require(:doc).permit(:title, :content)
 end
 
 end
